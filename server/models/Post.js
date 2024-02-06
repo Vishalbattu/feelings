26df22e3dfd712270d64d186fd8281143124bec9
Vishalbattu <81485041+Vishalbattu@users.noreply.github.com>
2024-02-06 20:44:10 +0000

    Update Post.js

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,1 +1,13 @@
 
+// server/models/Post.js
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
