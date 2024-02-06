b44dfd5c85726238f254a5c97841b1b56bb8fd1e
Vishalbattu <81485041+Vishalbattu@users.noreply.github.com>
2024-02-06 22:44:33 +0000

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,12 @@
+// server/index.js
+const express = require('express');
+const app = express();
+const port = 3001; // You can use any port you prefer
+
+app.get('/', (req, res) => {
+  res.send('Hello from the server!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
