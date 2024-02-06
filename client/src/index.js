422e74cdc3148960b1e7a8eccb9b8834e1893e9b
Vishalbattu <81485041+Vishalbattu@users.noreply.github.com>
2024-02-06 22:05:30 +0000

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,12 @@
-const React = require('react');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, Feelings App!</h1>
+    </div>
+  );
+};
 
+ReactDOM.render(<App />, document.getElementById('root'));
