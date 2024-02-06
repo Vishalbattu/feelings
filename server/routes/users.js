9c13187d108c82ccc8cd364d02429ba067cd1292
Vishalbattu <81485041+Vishalbattu@users.noreply.github.com>
2024-02-06 20:44:36 +0000

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,1 +1,20 @@
 
+// server/routes/users.js
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+router.get('/', (req, res) => {
+  User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+router.post('/add', (req, res) => {
+  const newUser = new User(req.body);
+  newUser.save()
+    .then(() => res.json('User added!'))
+    .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+module.exports = router;
