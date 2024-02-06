9194343aff64756a8375a0e8211b76c9c6d9927b
Vishalbattu <81485041+Vishalbattu@users.noreply.github.com>
2024-02-06 20:43:51 +0000

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,12 @@
 
+// server/models/User.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
