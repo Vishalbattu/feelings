f0653cb957e9545bdf47d44135bde0f8b9e72f43
Vishalbattu <81485041+Vishalbattu@users.noreply.github.com>
2024-02-06 20:45:11 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,32 @@
 
+// server/server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/feelings', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// API routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/posts', require('./routes/posts'));
+
+// Handle React routing, return all requests to React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
