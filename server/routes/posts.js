0c3d10091629b50d3d77a556ff15466a04798c26
Vishalbattu <81485041+Vishalbattu@users.noreply.github.com>
2024-02-06 20:44:53 +0000

    Update posts.js

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,1 +1,20 @@
+// server/routes/posts.js
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+router.get('/', (req, res) => {
+  Post.find()
+    .then(posts => res.json(posts))
+    .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+router.post('/add', (req, res) => {
+  const newPost = new Post(req.body);
+  newPost.save()
+    .then(() => res.json('Post added!'))
+    .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+module.exports = router;
 
