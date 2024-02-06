e3026d04b2c9c1559376c4928ddc80ceaf275620
Vishalbattu <81485041+Vishalbattu@users.noreply.github.com>
2024-02-06 22:02:08 +0000

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,3 @@
-// client/src/index.js
-//import React from 'react';
 const React = require('react');
-import ReactDOM from 'react-dom';
-import App from './components/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
